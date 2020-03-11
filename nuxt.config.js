6bef866eecb598f823b920214b4e456b2cf7dcda
MSayib <sayib.roziq2@gmail.com>
2020-03-11 22:05:28 +0700

    percobaan berhasil ambil data dari API lokal pertama

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -79,7 +79,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,16 +1,119 @@
 <template>
-  <v-app id="login">
+  <div>
     <h1>Data List</h1>
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4"></v-col>
-          <!-- isi konten disini -->
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+    <nuxt-link to="/data/create">
+      <button>tambah data</button>
+    </nuxt-link>
+    <b-form-input type="text" v-model="cari" placeholder="cari disini"></b-form-input>
+    <!-- <b-button variant="primary" @click="submit">Search</b-button> -->
+    <p v-if="isError">error</p>
+    <p v-else-if="isEmpty">tidak ditemukan</p>
+    <table v-else-if="!isLoading" border="1 solid">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>isi body</th>
+          <th>userID</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ListItem
+          v-for="data in datas"
+          :id="String(data.id)"
+          :key="String(data.id)"
+          :title="data.title"
+          :body="data.body"
+          :userId="String(data.userId)"
+          @refresh-dong="getData"
+        ></ListItem>
+      </tbody>
+    </table>
+    <b-spinner v-else label="Spinning"></b-spinner>
+    <!-- <b-table striped hover :items="datas"></b-table> -->
+  </div>
 </template>
 
 <script>
+import Axios from "axios";
+import ListItem from "~/components/list-item";
+
+export default {
+  components: { ListItem },
+  data() {
+    return {
+      datas: [],
+      isError: false,
+      isEmpty: false,
+      isLoading: false,
+      cari: ""
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    //   submit: function(){
+    //       this.getCari()
+    //   },
+    //   nyari: function(){
+    //       cari = this.isicari
+    //   },
+    async getData() {
+      this.isLoading = true;
+
+      try {
+        const res = await Axios.get("http://localhost:3001/datas");
+        this.datas = res.data;
+
+        if (this.datas.length === 0) {
+          this.isEmpty = true;
+        }
+      } catch (err) {
+        console.error(err);
+
+        this.isError = true;
+      }
+
+      this.isLoading = false;
+
+      // STYLE JADUL
+      // Axios.get('https://jsonplaceholder.typicode.com/datas')
+      //   .then((res) => {
+      //     this.datas = res.data
+
+      //     if (this.datas.length === 0) {
+      //       this.isEmpty = true
+      //     }
+
+      //     this.isLoading = false
+      //   })
+      //   .catch((err) => {
+      //     console.error(err)
+
+      //     this.isError = true
+      //     this.isLoading = false
+      //   })
+    },
+    // kode muklas
+    // computed:{
+    //     filteredPosts: function(){
+    //         return this.posts.filter((post) => {
+    //             return post.title.match(this.cari);
+    //         });
+    //     },
+    // }
+
+    getCari() {
+      this.$axios
+        .get("http://localhost:3001/datas/?q=" + this.cari)
+        .then(() => {
+          this.$emit("");
+        });
+    }
+  }
+};
 </script>
+
+<style scoped></style>
